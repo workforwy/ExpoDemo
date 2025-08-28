9062aa1a986a8eb6d3e24203d202ed1046060ebf
erik <wyforme88@gmail.com>
2025-08-28 15:29:00 +0800

    去掉冗余代码

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,60 +1,57 @@
-import * as Sentry from "@sentry/react-native";
+import {useColorScheme} from "@/hooks/useColorScheme";
+import {DarkTheme, DefaultTheme, ThemeProvider} from "@react-navigation/native";
+import {useFonts} from "expo-font";
 import {Stack} from "expo-router";
-import {useEffect} from "react";
+import {StatusBar} from "expo-status-bar";
+import "react-native-reanimated";
 import ComTitle from "./components/ComTitle";
 import LeftBtn from "./components/LeftBtn";
 
 export default function RootLayout() {
-  function initSentry() {
-    Sentry.init({
-      dsn: "https://examplePublicKey@o0.ingest.sentry.io/0",
-      // Set tracesSampleRate to 1.0 to capture 100%
-      // of transactions for tracing.
-      // We recommend adjusting this value in production
-      tracesSampleRate: 1.0,
-    });
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+  });
+
+  if (!loaded) {
+    // Async font loading only occurs in development.
+    return null;
   }
 
-  useEffect(() => {
-    initSentry();
-  }, []);
-
   return (
-    <Stack
-      screenOptions={{
-        headerLeft: (props) => {
-          return <LeftBtn />;
-        },
-        headerTitle: (props: any) => {
-          return <ComTitle props={props} />;
-        },
-      }}
-    >
-      <Stack.Screen
-        name="tabs"
-        options={{
-          headerShown: false,
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack
+        screenOptions={{
+          headerLeft: (props) => {
+            return <LeftBtn />;
+          },
+          headerTitle: (props: any) => {
+            return <ComTitle props={props} />;
+          },
         }}
-      />
-      <Stack.Screen
-        name="drawer"
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="login"
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="modal"
-        options={{
-          presentation: "formSheet",
-        }}
-      />
-      <Stack.Screen name="selectPic" />
-    </Stack>
+      >
+        <Stack.Screen name="(tabs)" options={{headerShown: false}} />
+        <Stack.Screen name="+not-found" />
+        <Stack.Screen
+          name="drawer"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="login"
+          options={{
+            headerShown: false,
+          }}
+        />
+        {/* <Stack.Screen
+          name="modal"
+          options={{
+            presentation: "formSheet",
+          }}
+        /> */}
+      </Stack>
+      <StatusBar style="auto" />
+    </ThemeProvider>
   );
 }
--- app/utils/logUtils.ts
+++ app/utils/logUtils.ts
@@ -1,32 +1,4 @@
-/**
- * 日志管理
- */
-
-// import Config from "react-native-config";
-
-// 线上日志处理
-// import * as Sentry from "@sentry/react-native";
-
-// const isDebug = Config.ENVIRONMENT === 'DEV';
-// jest.mock("os", () => require("./mock-os"));
-// import winston from "winston";
-// import RNFileTransport from "../RNFileTransport";
-
 import logger from "loglevel";
-
-// 创建logger
-// const logger = winston.createLogger({
-//   format: winston.format.combine(
-//     winston.format.timestamp(),
-//     winston.format.printf(({timestamp, level, message}) => {
-//       return `${timestamp} [${level}]: ${message}`;
-//     })
-//   ),
-//   transports: [
-//     new winston.transports.Console(), // 控制台日志输出
-//     new RNFileTransport({filename: "8u8game.log"}), // 文件日志输出
-//   ],
-// });
 
 const logLevel = __DEV__ ? "debug" : "info";
 
